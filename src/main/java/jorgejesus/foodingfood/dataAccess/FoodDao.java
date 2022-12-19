e0c8638241ee0464dcd3966613c3d9c0cb4237af
Şevket Emin Gürbüz <sevketemingurbuz6@gmail.com>
2022-12-19 11:49:49 +0300

    ignoreCase

--- src/main/java/jorgejesus/foodingfood/dataAccess/FoodDao.java
+++ src/main/java/jorgejesus/foodingfood/dataAccess/FoodDao.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import jorgejesus.foodingfood.entities.Food;
 
@@ -16,7 +14,7 @@ public interface FoodDao extends JpaRepository<Food, Integer>{
 	List<Food> getByFoodNameContainsIgnoreCase (String foodName);
 	
 
-	List<Food> getByFoodNameContains (String foodName, Pageable page);
+	List<Food> getByFoodNameContainsIgnoreCas (String foodName, Pageable page);
 	
 
 }
